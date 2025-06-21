5544f4dd446852c19944764e0996d616ee129a4a
mesren <themanhimself@mesren2.xyz>
2025-06-21 15:08:53 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/mesadev-esc.github.io/', // or your actual repo name
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
