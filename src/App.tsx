cfe7329669ced003557a698f9185aef0a532c6f0
mesren <themanhimself@mesren2.xyz>
2025-07-06 12:40:11 -0600

    fix att #2

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import SilvyrAuthSuccess from "./pages/silvyr/auth-success";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import SilvyrAuthSuccess from './pages/silvyr/auth-success';
 
 const queryClient = new QueryClient();
 
@@ -14,14 +14,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="/silvyr/auth-success" element={<SilvyrAuthSuccess />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
